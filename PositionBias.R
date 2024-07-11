bcc739fe09cc2d9911fbc8320ce30ed76ee88a19
Rowland Luo <rowlandluo0713@gmail.com>
2024-07-11 15:19:08 +0100

    Boxplot of objects of Interest.

--- PositionBias.R
+++ PositionBias.R
@@ -4,3 +4,12 @@ PositionBias <- PositionBias %>%
   select(23:24)
 boxplot(Frames ~ FramePosition, data = PositionBias, main = "Boxplot of Interest at 2 Positions", xlab = "Positions")
 t.test(Frames ~ FramePosition, data = PositionBias)
+
+#Now I check if all objects are about equally interesting.
+ObjectInterest <- read_csv("Bonsai-IsaRow - Object-in-context.csv")
+ObjectInterest <- ObjectInterest %>%
+  select(26:27)
+boxplot(Frames1 ~ Objects, data = ObjectInterest)
+one.way <- aov(Frames1 ~ Objects, data = ObjectInterest)
+summary(one.way)
+#So the green jar is not significantly less interesting than other objects.
