d77230481c80b3d713309ea9cdfaecbd794da0f3
Rowland Luo <rowlandluo0713@gmail.com>
2024-07-15 17:15:39 +0100

    Updated!

--- r1569/r1569.R
+++ r1569/r1569.R
@@ -3,7 +3,7 @@ library(tidyverse)
 SimilarLearn <- read_csv("240705_r1569_0_similar_learn.csv")
 SimilarLearn <- SimilarLearn %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>71) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x1 <- c(SimilarLearn$pixelx)
@@ -33,7 +33,7 @@ print(points_in_circle2)
 BaselineLearn <- read_csv("240705_r1569_1_baseline_learn.csv")
 BaselineLearn <- BaselineLearn %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>71) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x2 <- c(BaselineLearn$pixelx)
@@ -46,7 +46,6 @@ p2circle <- p2 + geom_circle(aes(x0 = 324, y0 = 710, r = 125), color = "red")
 p2circle <- p2circle + geom_circle(aes(x0 = 788, y0 = 266, r = 125), color = "red")
 p2circle <- p2circle +
   ggtitle("baseline")
-p2circle
 radius <- 125
 center_x1 <- 324
 center_y1 <- 710
@@ -62,7 +61,7 @@ print(points_in_circle2)
 BaselineTest <- read_csv("240705_r1569_2_baseline_test.csv")
 BaselineTest <- BaselineTest %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>0) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x3 <- c(BaselineTest$pixelx)
@@ -75,7 +74,6 @@ p3circle <- p3 + geom_circle(aes(x0 = 324, y0 = 705, r = 125), color = "red")
 p3circle <- p3circle + geom_circle(aes(x0 = 788, y0 = 271, r = 125), color = "blue")
 p3circle <- p3circle +
   ggtitle("baseline")
-p3circle
 radius <- 125
 center_x3 <- 324
 center_y3 <- 705
@@ -91,7 +89,7 @@ print(points_in_circle3)
 BaselineLearn1 <- read_csv("240705_r1569_3_baseline_learn.csv")
 BaselineLearn1 <- BaselineLearn1 %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>38) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x4 <- c(BaselineLearn1$pixelx)
@@ -104,7 +102,6 @@ p4circle <- p4 + geom_circle(aes(x0 = 319, y0 = 705, r = 125), color = "green")
 p4circle <- p4circle + geom_circle(aes(x0 = 793, y0 = 266, r = 125), color = "green")
 p4circle <- p4circle +
   ggtitle("baseline")
-p4circle
 radius <- 125
 center_x4 <- 319
 center_y4 <- 705
@@ -120,7 +117,7 @@ print(points_in_circle4)
 DifferentLearn <- read_csv("240705_r1569_4_different_learn.csv")
 DifferentLearn <- DifferentLearn %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>0) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x5 <- c(DifferentLearn$pixelx)
@@ -133,7 +130,6 @@ p5circle <- p5 + geom_circle(aes(x0 = 452, y0 = 989, r = 125), color = "purple")
 p5circle <- p5circle + geom_circle(aes(x0 = 1030, y0 = 452, r = 125), color = "purple")
 p5circle <- p5circle +
   ggtitle("different")
-p5circle
 radius <- 125
 center_x5 <- 452
 center_y5 <- 989
@@ -149,7 +145,7 @@ print(points_in_circle6)
 DifferentTest <- read_csv("240705_r1569_5_different_test.csv")
 DifferentTest <- DifferentTest %>%
   select(!15:24) %>%
-  filter(Value.Item1.Item2>260) %>%
+  filter(Value.Item1.Item2>0) %>%
   rename(pixelx = Value.Item1.Item3.X, pixely = Value.Item1.Item3.Y, TimePoint = Value.Item1.Item2) %>%
   select(!1, !14)
 x6 <- c(DifferentTest$pixelx)
@@ -162,7 +158,6 @@ p6circle <- p6 + geom_circle(aes(x0 = 452, y0 = 989, r = 125), color = "purple")
 p6circle <- p6circle + geom_circle(aes(x0 = 1035, y0 = 452, r = 125), color = "green")
 p6circle <- p6circle +
   ggtitle("different")
-p6circle
 radius <- 125
 center_x6 <- 452
 center_y6 <- 989
